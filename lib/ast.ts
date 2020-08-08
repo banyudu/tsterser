d57a20812a62dee1812ecea7c7576e26312a44c9
Yudu Ban <banyudu@gmail.com>
2020-08-08 23:54:05 +0800

    refactor: convert AST_TemplateSegment to class defination

--- lib/ast.ts
+++ lib/ast.ts
@@ -3252,23 +3252,31 @@ var AST_TemplateString: any = DEFNODE('TemplateString', ['segments'], {
 
 }, AST_Node)
 
-var AST_TemplateSegment: any = DEFNODE('TemplateSegment', ['value', 'raw'], {
-  drop_side_effect_free: return_null,
-  has_side_effects: return_false,
-  shallow_cmp: mkshallow({
-    value: 'eq'
-  }),
-  _size: function (): number {
+class AST_TemplateSegment extends AST_Node {
+  drop_side_effect_free = return_null
+  has_side_effects = return_false
+  shallow_cmp = mkshallow({ value: 'eq' })
+  _size (): number {
     return this.value.length
-  },
-  add_source_map: function (output) { output.add_mapping(this.start) }
-}, {
-  documentation: 'A segment of a template string literal',
-  propdoc: {
+  }
+
+  add_source_map (output) { output.add_mapping(this.start) }
+  static documentation = 'A segment of a template string literal'
+  static propdoc = {
     value: 'Content of the segment',
     raw: 'Raw content of the segment'
   }
-}, AST_Node)
+
+  CTOR = this.constructor
+  flags = 0
+  TYPE = 'TemplateSegment'
+  static PROPS = AST_Node.PROPS.concat(['value', 'raw'])
+  constructor (args?) { // eslint-disable-line
+    super(args)
+    this.value = args.value
+    this.raw = args.raw
+  }
+}
 
 /* -----[ JUMPS ]----- */
 
