d72ab8129ca4d3c114257662927727601403c370
Yudu Ban <banyudu@gmail.com>
2020-08-09 02:14:31 +0800

    refactor: convert AST_Debugger to class defination

--- lib/ast.ts
+++ lib/ast.ts
@@ -497,22 +497,30 @@ var AST_Statement: any = DEFNODE('Statement', null, {
   documentation: 'Base class of all statements'
 }, AST_Node)
 
-var AST_Debugger: any = DEFNODE('Debugger', null, {
-  _optimize: function (self, compressor) {
+class AST_Debugger extends AST_Statement {
+  _optimize (self, compressor) {
     if (compressor.option('drop_debugger')) { return make_node(AST_EmptyStatement, self) }
     return self
-  },
-  shallow_cmp: pass_through,
-  _size: () => 8,
-  _to_mozilla_ast: () => ({ type: 'DebuggerStatement' }),
-  _codegen: function (_self, output) {
+  }
+
+  shallow_cmp = pass_through
+  _size = () => 8
+  _to_mozilla_ast = () => ({ type: 'DebuggerStatement' })
+  _codegen (_self, output) {
     output.print('debugger')
     output.semicolon()
-  },
-  add_source_map: function (output) { output.add_mapping(this.start) }
-}, {
-  documentation: 'Represents a debugger statement'
-}, AST_Statement)
+  }
+
+  add_source_map (output) { output.add_mapping(this.start) }
+  static documentation = 'Represents a debugger statement'
+  CTOR = this.constructor
+  flags = 0
+  TYPE = 'Debugger'
+  static PROPS = AST_Statement.PROPS
+  constructor (args?) { // eslint-disable-line
+    super(args)
+  }
+}
 
 class AST_Directive extends AST_Statement {
   _optimize (self, compressor) {
